b346319366609452a50556f00f3a671a42b18e21
pavel <karpoffpasha@yandex.ru>
2026-04-03 18:57:16 +0200

    refactor: make startup and migrations explicit

--- src/database_control/postgres/__init__.py
+++ src/database_control/postgres/__init__.py
@@ -1,16 +1,26 @@
 from src.database_control.postgres.db import AsyncSessionLocal as AsyncSessionLocal
 from src.database_control.postgres.db import async_engine as async_engine
+from src.database_control.postgres.db import (
+    build_alembic_config as build_alembic_config,
+)
 from src.database_control.postgres.db import get_db as get_db
 from src.database_control.postgres.db import get_db_session as get_db_session
 from src.database_control.postgres.db import get_sessionmaker as get_sessionmaker
-from src.database_control.postgres.db import init_models as init_models
+from src.database_control.postgres.db import (
+    upgrade_database_head as upgrade_database_head,
+)
+from src.database_control.postgres.db import (
+    upgrade_database_head_async as upgrade_database_head_async,
+)
 
 
 __all__ = [
     "AsyncSessionLocal",
     "async_engine",
+    "build_alembic_config",
     "get_db",
     "get_db_session",
     "get_sessionmaker",
-    "init_models",
+    "upgrade_database_head",
+    "upgrade_database_head_async",
 ]
